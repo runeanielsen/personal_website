9957be3ee15a6c0340c97f8782017bb6f2ddd12a
Rune Nielsen <runeanielsen@live.dk>
2020-05-29 00:06:11 +0200

    updates deploy file

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,16 @@ set -e
 
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
+# Go To Public folder
+cd public
+
+# Cleanup to avoid dead files
+rm -r ./blogpost ./categories ./css ./page ./posts ./tags ./index.html ./index.xml ./sitemap.xml ./favicon.ico
+
+
+# Go To Main folder
+cd ..
+
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
